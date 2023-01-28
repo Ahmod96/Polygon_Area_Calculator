2a3e3f57b431b545327da5f3590c6073c2e2e92d
Ahmodu Oladejo <oladejoahmodu96@gmail.com>
2023-01-28 23:41:00 +0100

    Initial Commit

---
+++ Polygon_Area_Calculator.py
@@ -0,0 +1,55 @@
+class Rectangle():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        
+    def set_width(self, width):
+        self.width = width
+    
+    def set_height(self, height):
+        self.height = height
+    
+    def get_area(self):
+        return self.width * self.height
+    
+    def get_perimeter(self):
+        return 2 * self.width + 2 * self.height
+    
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** .5
+    
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return 'Too big for picture.'
+        else:
+            return (('*'*self.width +'\n') * self.height)
+        
+    def get_amount_inside(self, shape):
+        if shape.width > self.width or shape.height > self.height:
+            return 0
+        else:
+            return (self.height//shape.height) * (self.width//shape.width)
+    
+    def __str__(self):
+        return 'Rectangle(width=' + str(self.width) + ', height=' + str(self.height) +')'
+  
+class Square(Rectangle):
+    def __init__(self, side_lenght):
+        self.side_lenght = side_lenght
+        super().__init__(self.side_lenght, self.side_lenght)
+        
+    def set_side(self, side):
+        self.width = side
+        self.height = side
+        
+    def set_width(self, width):
+        super().set_width(width)
+        self.height = width
+    
+    def set_height(self, height):
+        super().set_height(height)
+        self.width= height
+    
+    def __str__(self):
+        super().__str__()
+        return 'Square(side=' + str(self.height) + ')'
